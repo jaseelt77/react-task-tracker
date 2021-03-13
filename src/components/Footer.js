b225fca8743a8a5d4102334f4b429babdc8f1ed0
JASEEL T <32891706+jaseel7@users.noreply.github.com>
2021-03-13 15:41:29 +0530

    task tracker

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+const footer = () => {
+    return (
+        <footer>
+         <p>copyright &copy; 2021</p>   
+         <Link to='/about'>About</Link>
+     
+     
+        </footer>
+            
+       
+    )
+}
+
+export default footer
+
